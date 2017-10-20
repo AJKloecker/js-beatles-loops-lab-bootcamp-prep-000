b2321a167ba8647bdef8d232176f6520cb33f2e4
Andrew K <ajkloecker@outlook.com>
2017-10-20 00:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 function theBeatlesPlay(musicians, instruments){
   var temp = [];
   var i = 0
-  for(i; i , musicians.length - 1; ++i){
+  for(i; i < musicians.length; ++i){
     temp.push(`${musicians[i]} plays ${musicians[i]} `)
   }
   return temp
 }
 
 function johnLennonFacts(facts){
-  
+  var i = 0
+  var temp = [];
+  while(i < musicians.length){
+    temp = `${facts[i]}!`
+    ++i
+  }
+  return temp
+}
+
+function iLoveTheBeatles(n){
+  var temp = [];
+  var i = 0
+  do{
+    temp.push("I love the Beatles!")
+  }while(i < n);
 }
