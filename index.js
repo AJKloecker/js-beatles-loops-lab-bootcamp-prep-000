f1528d4c88ed1b6055762869b26d47f926b056eb
Andrew K <ajkloecker@outlook.com>
2017-10-20 00:01:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function theBeatlesPlay(musicians, instruments){
+  var temp = [];
+  var i = 0
+  for(i; i , musicians.length - 1; ++i){
+    temp.push(`${musicians[i]} plays ${musicians[i]} `)
+  }
+  return temp
+}
+
+function johnLennonFacts(facts){
+  
+}
